2ed44a66b21a5af3ff6bf89e0740bfdc7c7894d8
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-01 12:48:33 +0530

    feat: :sparkles: all prompts fetched in home page

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -29,7 +29,7 @@ const handler = NextAuth({
           await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.image,
+            image: profile.picture,
           });
         }
 
