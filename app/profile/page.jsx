a2c237172a2fd1e087697a4125925d5a0a8f9761
Dexter <71167911+soham-basak@users.noreply.github.com>
2023-09-01 15:52:28 +0530

    feat: :sparkles: edit & delete prompt

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,16 +2,13 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Profile from "@/components/Profile";
 
 const MyProfile = () => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
-
-  const handleEdit = () => {};
-
-  const handleDelete = async () => {};
+  const Router = useRouter();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -21,6 +18,29 @@ const MyProfile = () => {
     };
     if (session?.user.id) fetchPosts();
   }, []);
+
+  const handleEdit = (post) => {
+    Router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        const filterPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filterPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <div>
